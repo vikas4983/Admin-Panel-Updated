f69b2e6e571251a653d2f9f56161b9c68dc798dc
vikas4983 <vikaskumarofficial2023@gmail.com>
2024-08-03 13:54:10 +0530

    Login OTP

--- app/Http/Controllers/admin_auth/MobileLoginController.php
+++ app/Http/Controllers/admin_auth/MobileLoginController.php
@@ -15,32 +15,26 @@ class MobileLoginController extends Controller
 {
     protected $smsApiService;
     protected $phoneNumberService;
-
-
     public function __construct(smsApiService $smsApiService, phoneNumberService $phoneNumberService)
     {
-        // Use camelCase for the property name
         $this->smsApiService = $smsApiService;
         $this->phoneNumberService = $phoneNumberService;
     }
-
     public function sendOtp(Request $request)
-    { 
+    {
         $validateRequest = $request->validate([
             'mobile' => ['required', 'digits:10'],
         ]);
         $number = $validateRequest['mobile'];
         $findadmin = Admin::where('mobile', $number)->first();
-        //dump($findadmin);
+
         if (!$findadmin) {
             return back()->with('error', "Admin not found!");
         }
-        $otp = rand(1000, 9999); // Generate a 4-digit OTP
-        
-        try {
+        $otp = rand(1000, 9999);
 
-           
-            $currentDateTime = Carbon::now()->addMinutes(5); // Set OTP expiry time to 5 minutes from now
+        try {
+            $currentDateTime = Carbon::now()->addMinutes(5);
             MobileLogin::create([
                 'admin_id' => $findadmin->id,
                 'otp' => $otp,
@@ -63,35 +57,28 @@ class MobileLoginController extends Controller
             return response()->json(['success' => false, 'message' => $e->getMessage()], 500);
         }
     }
-
-
-
     public function verifyOtp(Request $request)
     {
         $request->validate([
             'mobile' => 'required|digits:10',
             'otp' => 'required|digits:4',
         ]);
-
         $mobileLogin = MobileLogin::where('mobile', $request->mobile)
             ->where('otp', $request->otp)
             ->where('expires_at', '>', now())
             ->first();
-
         if (!$mobileLogin) {
             $admin = Admin::with(['otps' => function ($query) {
                 $query->latest('id')->first();
             }])->first();
-            //return redirect('admin-login')->with('error', 'Invalid OTP or OTP expired');
+
             return view('admin.admins.verify_otp', [
                 'admin' => $admin,
                 'error' => 'Invalid OTP or OTP expired'
             ]);
         }
-
         $admin = Admin::where('mobile', $request->mobile)->first();
         Auth::guard('admin')->login($admin);
-
         return redirect('admin/dashboard')->with('success', 'Logged in successfully');
     }
 
